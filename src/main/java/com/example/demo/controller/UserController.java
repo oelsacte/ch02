07eee0461e09c76cc82bbea121a63d9de5ee8334
ELSACTE PALMA,OCTAVIO <oelsacte@gmail.com>
2017-07-02 22:41:23 -0500

    Se modifica controller y se agrega RequestMapping a nivel de clase

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,17 +1,27 @@
 package com.example.demo.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import com.example.demo.model.User;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
 
-	@ResponseStatus(HttpStatus.NOT_IMPLEMENTED)
-	@RequestMapping(value = "/users", method = RequestMethod.GET)
-	void getAll() {
+	@RequestMapping(method = RequestMethod.GET)
+	List<User> getAll() {
+		User jane = new User(1L, "Jane");
+		User john = new User(2L, "John");
+		return Arrays.asList(jane, john);
 	}
 
 	@ResponseStatus(HttpStatus.NOT_IMPLEMENTED)
